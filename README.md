b690ba7c1bbbbe2b72a0eaec5e4dca307cb9fb1c
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-03 16:20:33 +0800

    link-to-markdown.js: add context menu

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## link-to-markdown.js
 
-Get the link and title of current page, convert them to Markdown link, and write to the clipboard.
+Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in userscript manager's menu.
 
-获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。
+获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
--- scripts/link-to-markdown.js
+++ scripts/link-to-markdown.js
@@ -2,20 +2,30 @@
 // @name        Link to Markdown
 // @namespace   https://github.com/CyrusYip/userscripts
 // @match       *://*/*
-// @grant       GM_setClipboard
-// @version     1.1
+// @grant       GM.setClipboard
+// @grant       GM.registerMenuCommand
+// @version     2.0
 // @author      Cyrus Yip
-// @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L .
-// @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L。
+// @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in userscript manager's menu.
+// @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
 // ==/UserScript==
 (function () {
+
+  let title, url, markdownLink
+  const writeLink = function () {
+    title = document.title
+    url = window.location.href
+    markdownLink = `[${title}](${url})`
+    GM.setClipboard(markdownLink)
+    console.log(`Markdown link:\n${markdownLink}`)
+  }
+
   document.addEventListener("keydown", ({ altKey, shiftKey, key }) => {
     if (altKey && shiftKey && key === 'L') {
-      const title = document.title
-      const url = window.location.href
-      const markdownLink = `[${title}](${url})`
-      GM_setClipboard(markdownLink)
-      console.log(`Markdown link:\n${markdownLink}`)
+      writeLink()
     }
   })
+
+  GM.registerMenuCommand('Copy Markdown link', writeLink)
+
 })();
