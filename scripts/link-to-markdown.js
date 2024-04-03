c70b612ac104fe8922ee28aa88ff498a73a43062
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-03 15:41:09 +0800

    link-to-markdown.js: listen to keydown event

--- scripts/link-to-markdown.js
+++ scripts/link-to-markdown.js
@@ -3,19 +3,19 @@
 // @namespace   https://github.com/CyrusYip/userscripts
 // @match       *://*/*
 // @grant       GM_setClipboard
-// @version     1.0
+// @version     1.1
 // @author      Cyrus Yip
-// @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard.
-// @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。
+// @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L .
+// @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L。
 // ==/UserScript==
 (function () {
-  const title = document.title
-  const url = window.location.href
-  const markdownLink = `[${title}](${url})`
-  document.onkeyup = (e) => {
-    if (e.code === 'KeyL' && e.altKey && e.shiftKey) {
+  document.addEventListener("keydown", ({ altKey, shiftKey, key }) => {
+    if (altKey && shiftKey && key === 'L') {
+      const title = document.title
+      const url = window.location.href
+      const markdownLink = `[${title}](${url})`
       GM_setClipboard(markdownLink)
       console.log(`Markdown link:\n${markdownLink}`)
     }
-  }
+  })
 })();
