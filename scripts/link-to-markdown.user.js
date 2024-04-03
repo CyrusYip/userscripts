3077117fb6fc9c399a99c71d9af47c0a87a64201
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-03 23:03:30 +0800

    link-to-markdown: center text and limit max-width

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -4,7 +4,7 @@
 // @match       *://*/*
 // @grant       GM.setClipboard
 // @grant       GM.registerMenuCommand
-// @version     3.1
+// @version     4.0
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in the userscript manager's menu.
 // @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
@@ -22,24 +22,26 @@
   const showResult = () => {
     const messageDiv = document.createElement('div')
     messageDiv.innerHTML = markdownLink
-      ? `<p>Link was copied.</p>
+      ? `<h2>✅ Link was copied.</h2>
         <p>${markdownLink}</p>`
-      : `<p>Failed to get link.</p>`
+      : `<h2>❎ Failed to get link.</h2>`
     messageDiv.style.cssText = `
       position: fixed;
       top: 50%;
       left: 50%;
+      max-width: 50%;
+      text-align: center;
       transform: translate(-50%, -50%);
       background-color: rgba(0, 0, 0, 0.8);
       color: white;
       padding: 10px;
-      border-radius: 5px;
+      border-radius: 8px;
       z-index: 9999;
     `
     document.body.appendChild(messageDiv)
     setTimeout(() => {
       messageDiv.remove()
-    }, 2000)
+    }, 3000)
   }
 
   // keyboard shortcut
