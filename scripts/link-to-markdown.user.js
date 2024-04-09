bd89edd1648dab7bcaacf4f49b4cf0b00cd583aa
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-09 14:57:16 +0800

    link-to-markdown: use alert() instead of prompt()

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -4,7 +4,7 @@
 // @match       *://*/*
 // @grant       GM.setClipboard
 // @grant       GM.registerMenuCommand
-// @version     5.1
+// @version     5.2
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in the userscript manager's menu.
 // @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
@@ -20,9 +20,9 @@
 
   const showResult = () => {
     if (markdownLink) {
-      window.prompt(`✅ Link was copied.\n\n${markdownLink}`)
+      window.alert(`✅ Link was copied.\n\n${markdownLink}`)
     } else {
-      window.prompt(`❎ Failed to get link.`)
+      window.alert(`❎ Failed to get link.`)
     }
   }
 
