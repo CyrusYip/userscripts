ac9dfbcbe7b5bf82de62e55879aaa02eb54725ef
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-09 14:57:16 +0800

    link-to-markdown: use more meaningful function name

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -4,14 +4,14 @@
 // @match       *://*/*
 // @grant       GM.setClipboard
 // @grant       GM.registerMenuCommand
-// @version     5.0
+// @version     5.1
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in the userscript manager's menu.
 // @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
 // ==/UserScript==
 (() => {
   let title, url, markdownLink
-  const writeLink = () => {
+  const copyLink = () => {
     title = document.title
     url = window.location.href
     markdownLink = `[${title}](${url})`
@@ -29,10 +29,10 @@
   // keyboard shortcut: Shift + Alt + L
   document.addEventListener("keydown", ({ altKey, shiftKey, key }) => {
     if (altKey && shiftKey && key === 'L') {
-      writeLink()
+      copyLink()
       showResult()
     }
   })
 
-  GM.registerMenuCommand('Copy Markdown link', () => { writeLink(); showResult() })
+  GM.registerMenuCommand('Copy Markdown link', () => { copyLink(); showResult() })
 })();
