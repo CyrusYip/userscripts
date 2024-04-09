2ad4778158cafdc0cae5a96a34bec8fea8104aed
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-09 14:57:16 +0800

    link-to-markdown: use prompt() to avoid writing CSS

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -4,7 +4,7 @@
 // @match       *://*/*
 // @grant       GM.setClipboard
 // @grant       GM.registerMenuCommand
-// @version     4.1
+// @version     5.0
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in the userscript manager's menu.
 // @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
@@ -16,35 +16,17 @@
     url = window.location.href
     markdownLink = `[${title}](${url})`
     GM.setClipboard(markdownLink)
-    console.log(`Markdown link:\n${markdownLink}`)
   }
 
   const showResult = () => {
-    const messageDiv = document.createElement('div')
-    messageDiv.innerHTML = markdownLink
-      ? `<h2 style="color: inherit">✅ Link was copied.</h2>
-        <p style="color: inherit">${markdownLink}</p>`
-      : `<h2 style="color: inherit">❎ Failed to get link.</h2>`
-    messageDiv.style.cssText = `
-      position: fixed;
-      top: 50%;
-      left: 50%;
-      max-width: 50%;
-      text-align: center;
-      transform: translate(-50%, -50%);
-      background-color: rgba(0, 0, 0, 0.8);
-      color: white;
-      padding: 10px;
-      border-radius: 8px;
-      z-index: 9999;
-    `
-    document.body.appendChild(messageDiv)
-    setTimeout(() => {
-      messageDiv.remove()
-    }, 3000)
+    if (markdownLink) {
+      window.prompt(`✅ Link was copied.\n\n${markdownLink}`)
+    } else {
+      window.prompt(`❎ Failed to get link.`)
+    }
   }
 
-  // keyboard shortcut
+  // keyboard shortcut: Shift + Alt + L
   document.addEventListener("keydown", ({ altKey, shiftKey, key }) => {
     if (altKey && shiftKey && key === 'L') {
       writeLink()
