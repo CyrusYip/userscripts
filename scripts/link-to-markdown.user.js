74064e9ef111bcec231453db286be4d3e60596da
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-13 05:03:57 +0800

    feat: remove immediately invoked function

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -4,36 +4,34 @@
 // @match       *://*/*
 // @grant       GM.setClipboard
 // @grant       GM.registerMenuCommand
-// @version     5.3
+// @version     5.4
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in the userscript manager's menu.
 // @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
 // ==/UserScript==
 'use strict';
-(() => {
-  let title, url, markdownLink
-  const copyLink = () => {
-    title = document.title
-    url = window.location.href
-    markdownLink = `[${title}](${url})`
-    GM.setClipboard(markdownLink)
+let title, url, markdownLink
+const copyLink = () => {
+  title = document.title
+  url = window.location.href
+  markdownLink = `[${title}](${url})`
+  GM.setClipboard(markdownLink)
+}
+
+const showResult = () => {
+  if (markdownLink) {
+    window.alert(`✅ Link was copied.\n\n${markdownLink}`)
+  } else {
+    window.alert(`❎ Failed to get link.`)
   }
+}
 
-  const showResult = () => {
-    if (markdownLink) {
-      window.alert(`✅ Link was copied.\n\n${markdownLink}`)
-    } else {
-      window.alert(`❎ Failed to get link.`)
-    }
+// keyboard shortcut: Shift + Alt + L
+document.addEventListener("keydown", ({ altKey, shiftKey, key }) => {
+  if (altKey && shiftKey && key === 'L') {
+    copyLink()
+    showResult()
   }
+})
 
-  // keyboard shortcut: Shift + Alt + L
-  document.addEventListener("keydown", ({ altKey, shiftKey, key }) => {
-    if (altKey && shiftKey && key === 'L') {
-      copyLink()
-      showResult()
-    }
-  })
-
-  GM.registerMenuCommand('Copy Markdown link', () => { copyLink(); showResult() })
-})();
+GM.registerMenuCommand('Copy Markdown link', () => { copyLink(); showResult() })
--- scripts/remove-localstorage.user.js
+++ scripts/remove-localstorage.user.js
@@ -3,25 +3,23 @@
 // @namespace   https://github.com/CyrusYip/userscripts
 // @match       *://*/*
 // @grant       GM.registerMenuCommand
-// @version     1.2
+// @version     1.3
 // @author      Cyrus Yip
 // @description Remove localStorage of the current page. It's used for testing websites.
 // ==/UserScript==
 'use strict';
-(() => {
-  const main = () => {
-    // Check emptiness
-    if (localStorage.length === 0) {
-      alert('localStorage is empty now.')
-      return
-    }
-    // Remove localStorage
-    const items = JSON.stringify(localStorage, null, 2)
-    const message = `Would you like to remove localStorage?\n\n${items}`
-    if (window.confirm(message)) {
-      localStorage.clear()
-      main() // Check emptiness again
-    }
+const main = () => {
+  // Check emptiness
+  if (localStorage.length === 0) {
+    alert('localStorage is empty now.')
+    return
   }
-  GM.registerMenuCommand('Remove localStorage', main)
-})()
+  // Remove localStorage
+  const items = JSON.stringify(localStorage, null, 2)
+  const message = `Would you like to remove localStorage?\n\n${items}`
+  if (window.confirm(message)) {
+    localStorage.clear()
+    main() // Check emptiness again
+  }
+}
+GM.registerMenuCommand('Remove localStorage', main)
