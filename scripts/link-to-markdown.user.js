b508cfd63565544e03e27eee000ceefee17ff4fd
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-21 03:48:17 +0800

    link-to-markdown: shortcut can be disabled

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -2,9 +2,11 @@
 // @name        Link to Markdown
 // @namespace   https://github.com/CyrusYip/userscripts
 // @match       *://*/*
-// @grant       GM.setClipboard
 // @grant       GM.registerMenuCommand
-// @version     6.0.1
+// @grant       GM.setClipboard
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @version     7.0.0
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write to the clipboard. Shortcut: Shift + Alt + L . There is a button in the userscript manager's menu.
 // @description:zh-CN 获取当前页面的链接与标题，将其转换为 Markdown 链接，并写入剪贴板。快捷键：Shift + Alt + L，脚本管理器菜单也有按钮。
@@ -26,12 +28,19 @@ const showResult = () => {
   }
 }
 
+// set default value of 'disable-shortcut' as false
+if (GM_getValue('disable-shortcut') === undefined) {
+  GM_setValue('disable-shortcut', false)
+}
 // keyboard shortcut: Shift + Alt + L
-document.addEventListener("keydown", ({ altKey, shiftKey, ctrlKey, key }) => {
-  if (ctrlKey === false && altKey && shiftKey && key === 'L') {
-    copyLink()
-    showResult()
-  }
-})
+// to disable shortcut, set 'disable-shortcut' as true
+if (GM_getValue('disable-shortcut') !== true) {
+  document.addEventListener("keydown", ({ altKey, shiftKey, ctrlKey, key }) => {
+    if (ctrlKey === false && altKey && shiftKey && key === 'L') {
+      copyLink()
+      showResult()
+    }
+  })
+}
 
 GM.registerMenuCommand('Copy Markdown link', () => { copyLink(); showResult() })
