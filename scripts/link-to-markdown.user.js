2702cf50858ecc612ca80bcb724eaf666b438042
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-21 23:42:30 +0800

    link-to-markdown: compatible with Greasemonkey

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -4,10 +4,10 @@
 // @match       *://*/*
 // @grant       GM.registerMenuCommand
 // @grant       GM.setClipboard
-// @grant       GM_getValue
-// @grant       GM_setValue
+// @grant       GM.getValue
+// @grant       GM.setValue
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
-// @version     8.1.1
+// @version     8.2.1
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write it to the clipboard. To use this script, click the button in the userscript manager's menu or press Shift + Alt + L . This script can be customized in Violentmonkey (Dashboard -> Edit -> Values). To disable the shortcut, set 'disable-shortcut' as true. To change shortcut, set 'shortcut' (Key Definition: https://github.com/violentmonkey/vm-shortcut#key-definition). To reset, remove all values.
 // ==/UserScript==
@@ -29,13 +29,15 @@ const showResult = () => {
 }
 
 // set default value of 'disable-shortcut' as false, 'shortcut' as 'shift-alt-l'
-GM_getValue('disable-shortcut') === undefined && GM_setValue('disable-shortcut', false)
-GM_getValue('shortcut') === undefined && GM_setValue('shortcut', 'shift-alt-l')
-if (GM_getValue('disable-shortcut') !== true) {
-  VM.shortcut.register(GM_getValue('shortcut'), () => {
-    copyLink()
-    showResult()
-  })
-}
+(async () => {
+  await GM.getValue('disable-shortcut') === undefined && GM.setValue('disable-shortcut', false)
+  await GM.getValue('shortcut') === undefined && GM.setValue('shortcut', 'shift-alt-l')
+  if (await GM.getValue('disable-shortcut') !== true) {
+    VM.shortcut.register(await GM.getValue('shortcut'), () => {
+      copyLink()
+      showResult()
+    })
+  }
+})()
 
 GM.registerMenuCommand('Copy Markdown link', () => { copyLink(); showResult() })
