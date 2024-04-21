ad2a73d0ac67f0f34b76b47970003767aaf40379
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-21 23:42:30 +0800

    link-to-markdown: improve readability

--- scripts/link-to-markdown.user.js
+++ scripts/link-to-markdown.user.js
@@ -8,11 +8,18 @@
 // @grant       GM.setValue
 // @grant       GM.notification
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
-// @version     9.1.0
+// @version     9.2.0
 // @author      Cyrus Yip
 // @description Get the link and title of current page, convert them to Markdown link, and write it to the clipboard. To use this script, click the button in the userscript manager's menu or press Shift + Alt + L . This script can be customized in Violentmonkey (Dashboard -> Edit -> Values). To disable the shortcut, set 'disable-shortcut' as true. To disable notification, set 'disable-notification' as true. To change shortcut, set 'shortcut' (Key Definition: https://github.com/violentmonkey/vm-shortcut#key-definition). To reset, remove all values.
 // ==/UserScript==
 'use strict';
+
+const setDefaultValues = async () => {
+  await GM.getValue('disable-shortcut') === undefined && GM.setValue('disable-shortcut', false)
+  await GM.getValue('shortcut') === undefined && GM.setValue('shortcut', 'shift-alt-l')
+  await GM.getValue('disable-notification') === undefined && GM.setValue('disable-notification', false)
+}
+
 let title, href, hash, markdownLink
 const copyLink = () => {
   ({ title } = document);
@@ -22,24 +29,20 @@ const copyLink = () => {
 }
 
 const showResult = async () => {
-  if (await GM.getValue('disable-notification') !== true) {
-    markdownLink
-      ? GM.notification(`✔ Link was copied.\n${markdownLink}`)
-      : GM.notification(`✘ Failed to get link.`)
-  }
+  if (await GM.getValue('disable-notification') === true) { return }
+  markdownLink
+    ? GM.notification(`✔ Link was copied.\n${markdownLink}`)
+    : GM.notification(`✘ Failed to get link.`)
 }
 
-// set default values
-(async () => {
-  await GM.getValue('disable-shortcut') === undefined && GM.setValue('disable-shortcut', false)
-  await GM.getValue('shortcut') === undefined && GM.setValue('shortcut', 'shift-alt-l')
-  await GM.getValue('disable-notification') === undefined && GM.setValue('disable-notification', false)
-  if (await GM.getValue('disable-shortcut') !== true) {
-    VM.shortcut.register(await GM.getValue('shortcut'), () => {
-      copyLink()
-      showResult()
-    })
-  }
-})()
+const setShortcut = async () => {
+  if (await GM.getValue('disable-shortcut') === true) { return }
+  VM.shortcut.register(await GM.getValue('shortcut'), () => {
+    copyLink()
+    showResult()
+  })
+}
 
+setDefaultValues()
+setShortcut()
 GM.registerMenuCommand('Copy Markdown link', () => { copyLink(); showResult() })
